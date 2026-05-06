766c1a648847c704314d95eb5966ec12f9181523
Maka1010 <23402080059602@cecytebc.edu.mx>
2026-05-06 09:42:26 -0700

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -187,6 +187,7 @@ const styles = StyleSheet.create({
     marginTop: 14,
   },
   input: {
+    marginBottom: 20,
     backgroundColor: INPUT_BG,
     borderWidth: 1,
     borderColor: INPUT_BORDER,
